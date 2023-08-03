3097a7316ab4bbe7ed0bed31eb08611416807b48
StefanoCossu <cossu.stefano1984@gmail.com>
2023-08-03 20:24:03 +0200

    Occhio che è rotto

--- src/Contexts/searched.jsx
+++ src/Contexts/searched.jsx
@@ -1,13 +1,54 @@
-import { createContext,useState } from "react";
+import { createContext,useEffect,useState } from "react";
+import { detail } from "../Pages/Details";
 
 export const Context = createContext()
 
 export function ContextProvider(props) {
-    const [searched, setSearched]=useState("");
+    const [searched, setSearched] = useState("");
+    const [prefered,setPrefered] = useState([]);
+    // controllare se già presente qualcosa nel local storage
+    
+    useEffect(()=>{
+        if (localStorage.getItem("prefered")) { 
+            setPrefered(()=>JSON.parse(localStorage.getItem("prefered")))
+            console.log("sono qui");
+        }
+        // }else{
+        //     localStorage.setItem("prefered", JSON.stringify(prefered))
+        // } 
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem("prefered", JSON.stringify(prefered))
+        // console.log(prefered);
+    },[prefered])
+    const togglePrefered = (id) => {
+        // console.log(JSON.stringify(prefered));
+        // usiamo find/include/filter per vedere se l'id è nella lista dei favoriti
+        const isInArray = prefered.findIndex( el => el === id)
+        
+        if(isInArray === -1){
+            setPrefered((prev)=>[...prev,id])
+        }else{
+            setPrefered(prev => prev.toSpliced( isInArray, 1))
+            
+        }
+        // console.log(prefered);
+
+        localStorage.setItem("prefered", JSON.stringify(prefered))
+
+        // se non c'è array.push, setPrefered((prev)=>[...prev,id])
+        // se c'è setPrefered(prev => prev.toSpliced(indice che avrò dal punto 1,1)) 
+        
+        // Prima di andarmene local storage.setItem("prefered",preferd.toString/JSONS)
+        
+    }
+    
     
     const researched = (props) =>{
         setSearched(props)
     }
     
-    return <Context.Provider value={{searched , researched}}>{props.children}</Context.Provider>
+    // return <Context.Provider value={{searched , researched}}>{props.children}</Context.Provider>
+    return <Context.Provider value={{searched , researched, togglePrefered, prefered}}>{props.children}</Context.Provider>
 }
